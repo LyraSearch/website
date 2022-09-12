526e24411ee7f469a5ac4b4736b439137ceeb295
Jaga Santagostino <jagasantagostino@gmail.com>
2022-09-12 22:25:56 +0200

    wip

--- components/ShortDemo/index.tsx
+++ components/ShortDemo/index.tsx
@@ -1,128 +1,162 @@
-import Image from 'next/image'
-import { useEffect, useState } from 'react'
-import { Input } from '@chakra-ui/input'
-import { Box, Text } from '@chakra-ui/layout'
-import { create, insertBatch, RetrievedDoc, search, formatNanoseconds } from '@lyrasearch/lyra'
-import { Skeleton } from '@chakra-ui/skeleton'
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { Input } from "@chakra-ui/input";
+import { Box, Text } from "@chakra-ui/layout";
+import {
+  create,
+  insertBatch,
+  RetrievedDoc,
+  search,
+  formatNanoseconds,
+} from "@lyrasearch/lyra";
+import { Skeleton } from "@chakra-ui/skeleton";
+import { useBreakpointValue } from "@chakra-ui/react";
 
-function getDefaultSearchTerm () {
+function getDefaultSearchTerm() {
   const defaultSearchTerms = [
-    'music',
-    'school',
-    'avengers',
-    'love',
-    'nature',
-    'blue',
-    'dance',
-    'sun',
-    'christmas',
-    'colors',
-    'life'
-  ]
+    "music",
+    "school",
+    "avengers",
+    "love",
+    "nature",
+    "blue",
+    "dance",
+    "sun",
+    "christmas",
+    "colors",
+    "life",
+  ];
 
-  return defaultSearchTerms[Math.floor(Math.random() * defaultSearchTerms.length)]
+  return defaultSearchTerms[
+    Math.floor(Math.random() * defaultSearchTerms.length)
+  ];
 }
 
 const db = create({
   schema: {
-    title: 'string',
-    description: 'string',
-    image: 'string'
-  }
-})
+    title: "string",
+    description: "string",
+    image: "string",
+  },
+});
 
-function trimText (text: string): string {
+function trimText(text: string): string {
   if (text.length > 100) {
-    return text.substring(0, 100) + '...'
+    return text.substring(0, 100) + "...";
   }
 
-  return text
+  return text;
 }
 
 export const ShortDemo = () => {
-  const [ready, setReady] = useState(false)
-  const [term, setTerm] = useState(getDefaultSearchTerm())
-  const [results, setResults] = useState<Array<RetrievedDoc<any>>>([])
-  const [elapsed, setElapsed] = useState('0')
-  const [count, setCount] = useState(0)
+  const [ready, setReady] = useState(false);
+  const [term, setTerm] = useState(getDefaultSearchTerm());
+  /* TODO: remove any! */
+  const [results, setResults] = useState<Array<RetrievedDoc<any>>>([]);
+  const [elapsed, setElapsed] = useState("0");
+  const [count, setCount] = useState(0);
 
-  function performSearch (term: string) {
+  const numberOfResults = useBreakpointValue({
+    base: 2,
+    sm: 3,
+  });
+
+  const skeletonHeight = useBreakpointValue({
+    base: 45,
+    sm: 80,
+  });
+
+  function performSearch(term: string) {
     const searchData = search(db, {
       term,
-      limit: 3,
+      limit: numberOfResults,
       offset: 0,
       exact: false,
-      properties: ['title']
-    })
+      properties: ["title"],
+    });
 
-    setResults(searchData.hits)
-    setElapsed(formatNanoseconds(searchData.elapsed))
-    setCount(searchData.count)
+    setResults(searchData.hits);
+    setElapsed(formatNanoseconds(searchData.elapsed));
+    setCount(searchData.count);
   }
 
   useEffect(() => {
     if (ready) {
-      performSearch(term)
+      performSearch(term);
     }
-  }, [term])
+  }, [term]);
 
   useEffect(() => {
-    async function populate () {
-      const res = await fetch('/data/movies.json')
-      const movies = await res.json()
-      await insertBatch(db, movies)
-      setReady(true)
-      setTerm(getDefaultSearchTerm())
+    async function populate() {
+      const res = await fetch("/data/movies.json");
+      const movies = await res.json();
+      await insertBatch(db, movies);
+      setReady(true);
+      setTerm(getDefaultSearchTerm());
     }
 
-    populate()
-  }, [])
+    populate();
+  }, []);
 
   return (
     <Box>
-      <Box w='full' m='auto'>
+      <Box bg="pink" mt="12" mb="4">
+        SAY IS A DEMO HERE AND ADD A Very small (font) description
+        {/* TODO: use a different BG for demo to make it pop clear is another section */}
+      </Box>
+      <Box w="full" m="auto">
         <Input
           disabled={!ready}
           value={term}
           onChange={(ev) => setTerm(ev.target.value)}
-          m='auto'
-          type='text'
-          w='full'
-          placeholder='Search for a recent movie'
-          bgColor='whiteAlpha.500'
+          m="auto"
+          type="text"
+          w="full"
+          placeholder="Search for a recent movie"
+          bgColor="whiteAlpha.500"
         />
       </Box>
-
-      <Box textAlign='center' mt='4'>
+      <Box textAlign="center" mt="4">
         {count} total results in {elapsed}
       </Box>
-
-      <Box display='grid' gridTemplateColumns='repeat(3, 1fr)' gap='4' mt='4'>
-
-        {!ready && (
+      {/* TODO: Try to move the demo input below the results
+    WHY?: because with mobile keyboard open it can be better UX, not tried yet
+ */}
+      <Box
+        display="grid"
+        gridTemplateColumns={`repeat(${numberOfResults}, 1fr)`}
+        gap="4"
+        mt="4"
+      >
+        {!ready &&
           [1, 2, 3].map((i) => (
-            <Box key={i} p='4' h='80' rounded='lg' bgColor='white'>
-              <Skeleton rounded='md' w='full' h='full' />
+            <Box key={i} p="4" h={skeletonHeight} rounded="lg" bgColor="white">
+              <Skeleton rounded="md" w="full" h="full" />
             </Box>
-          ))
-        )}
+          ))}
 
         {results.map((result) => (
-          <Box key={result.id} p='4' rounded='lg' bgColor='white'>
-            <Box pos='relative' w='full' h='40' rounded='xl'>
+          <Box key={result.id} p="4" rounded="lg" bgColor="white">
+            <Box pos="relative" w="full" h="40" rounded="xl">
               <Image
                 src={`https://image.tmdb.org/t/p/w500/${result.image}`}
-                layout='fill'
-                objectFit='cover'
-                style={{ borderRadius: '0.5rem' }}
+                layout="fill"
+                objectFit="cover"
+                style={{ borderRadius: "0.5rem" }}
               />
             </Box>
 
-            <Text mt='3' maxW='full' color='gray.900' fontWeight='bold'> <>{result.title}</> </Text>
-            <Text color='gray.600' mt='2'> <>{trimText(result.description as string)}</> </Text>
+            <Text mt="3" maxW="full" color="gray.900" fontWeight="bold">
+              {" "}
+              <>{result.title}</>{" "}
+            </Text>
+            {/* <Text color="gray.600" mt="2">
+              {" "}
+              <>{trimText(result.description as string)}</>{" "}
+            </Text> */}
           </Box>
         ))}
       </Box>
     </Box>
-  )
-}
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,110 +1,190 @@
-import type { NextPage } from 'next'
-import { useEffect, useState } from 'react'
-import { Image, Tooltip } from '@chakra-ui/react'
-import { Box, Grid, GridItem, Heading, Text } from '@chakra-ui/layout'
-import { ShortDemo } from '../components/ShortDemo'
-import GitHubButton from 'react-github-btn'
+import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+import { Image, Show, Tooltip } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Heading, Text } from "@chakra-ui/layout";
+import { ShortDemo } from "../components/ShortDemo";
+import GitHubButton from "react-github-btn";
 
+/* TODO?: Add links to projects */
 const supportedRuntimes = [
   {
-    name: 'AWS Lambda@Edge',
-    image: '/misc/runtimes/aws-lambda-edge.svg'
+    name: "AWS Lambda@Edge",
+    image: "/misc/runtimes/aws-lambda-edge.svg",
   },
   {
-    name: 'Bun',
-    image: '/misc/runtimes/bun.svg'
+    name: "Bun",
+    image: "/misc/runtimes/bun.svg",
   },
   {
-    name: 'Cloudflare Workers',
-    image: '/misc/runtimes/cloudflare-workers.svg'
+    name: "Cloudflare Workers",
+    image: "/misc/runtimes/cloudflare-workers.svg",
   },
   {
-    name: 'Deno',
-    image: '/misc/runtimes/deno.svg'
+    name: "Deno",
+    image: "/misc/runtimes/deno.svg",
   },
   {
-    name: 'Browsers',
-    image: '/misc/runtimes/javascript.svg'
+    name: "Browsers",
+    image: "/misc/runtimes/javascript.svg",
   },
   {
-    name: 'Netlify Functions',
-    image: '/misc/runtimes/netlify-functions.svg'
+    name: "Netlify Functions",
+    image: "/misc/runtimes/netlify-functions.svg",
   },
   {
-    name: 'Node.js',
-    image: '/misc/runtimes/node-js.svg'
+    name: "Node.js",
+    image: "/misc/runtimes/node-js.svg",
   },
   {
-    name: 'React Native',
-    image: '/misc/runtimes/react-native.svg'
-  }
-]
+    name: "React Native",
+    image: "/misc/runtimes/react-native.svg",
+  },
+];
 
 const Home: NextPage = () => {
-  const definitions = ['edge', 'offline', 'embedded', 'universal']
-  const [definition, setDefinition] = useState(definitions[0])
+  const definitions = ["edge", "offline", "embedded", "universal"];
+  const [definition, setDefinition] = useState(definitions[0]);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      const index = definitions.indexOf(definition)
-      const nextIndex = index + 1 === definitions.length ? 0 : index + 1
-      setDefinition(definitions[nextIndex])
-    }, 3000)
+      const index = definitions.indexOf(definition);
+      const nextIndex = index + 1 === definitions.length ? 0 : index + 1;
+      setDefinition(definitions[nextIndex]);
+    }, 3000);
 
-    return () => clearInterval(interval)
-  }, [])
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <>
+    <Box>
+      <Show above="sm">
+        <Box
+          pos="absolute"
+          top="0"
+          right="0"
+          w="40%"
+          h="container.md"
+          bg="red"
+          zIndex="0"
+          bgGradient="linear(to-bl, #FF00E5, #5200FF, #00C8FF)"
+          roundedBottomLeft="20%"
+        />
+      </Show>
       <Box
-        pos='absolute'
-        top='0'
-        right='0'
-        w='40%'
-        h='container.md'
-        bg='red'
-        zIndex='0'
-        bgGradient='linear(to-bl, #FF00E5, #5200FF, #00C8FF)'
-        roundedBottomLeft='20%'
-      />
-      <Box w='container.xl' h='container.sm' m='auto' pt='40' display='grid' gridTemplateColumns='1fr 1fr' zIndex='1'>
-        <Box display='flex' flexDir='column' w='full' h='full' justifyContent='center'>
-          <Heading fontSize='6xl' color='gray.100' lineHeight='1'>
-            The <Text as='span' bgGradient='linear(to-l, #08B5FF, #F101E8)' bgClip='text'>{definition}</Text><br />
+        px={["4", "0"]}
+        /* h="container.sm" */ m="auto"
+        pt={["24", "40"]}
+        zIndex="1"
+      >
+        <Box
+          display="flex"
+          flexDir="column"
+          w="full"
+          h="full"
+          justifyContent="center"
+          label="XXXX"
+        >
+          <Heading fontSize="6xl" color="gray.100" lineHeight="1">
+            The{" "}
+            <Text
+              as="span"
+              bgGradient="linear(to-l, #08B5FF, #F101E8)"
+              bgClip="text"
+            >
+              {definition}
+            </Text>
+            <br />
             search experience
           </Heading>
-          <Text as='p' fontSize='xl' color='gray.400' mt='3'>
-            Lyra is a fully-featured full-text search engine that runs wherever JavaScript runs, including browsers, servers, React Native, edge networks, and more.
+          <Text as="p" fontSize="xl" color="gray.400" mt="3">
+            Lyra is a fully-featured full-text search engine that runs wherever
+            JavaScript runs, including browsers, servers, React Native, edge
+            networks, and more.
           </Text>
-          <Box display='flex' mt='5' w='full'>
+          <Box label="github buttons" display="flex" mt="5" w="full">
             <Box>
-              <GitHubButton href='https://github.com/nearform/lyra' data-icon='octicon-star' data-size='large' data-show-count='true' aria-label='Star nearform/lyra on GitHub'>Star</GitHubButton>
+              <GitHubButton
+                href="https://github.com/nearform/lyra"
+                data-icon="octicon-star"
+                data-size="large"
+                data-show-count="true"
+                aria-label="Star nearform/lyra on GitHub"
+              >
+                Star
+              </GitHubButton>
             </Box>
-            <Box ml='3'>
-              <GitHubButton href='https://github.com/nearform/lyra/fork' data-icon='octicon-repo-forked' data-size='large' data-show-count='true' aria-label='Fork nearform/lyra on GitHub'>Fork</GitHubButton>
+            <Box ml="3">
+              <GitHubButton
+                href="https://github.com/nearform/lyra/fork"
+                data-icon="octicon-repo-forked"
+                data-size="large"
+                data-show-count="true"
+                aria-label="Fork nearform/lyra on GitHub"
+              >
+                Fork
+              </GitHubButton>
             </Box>
           </Box>
         </Box>
-        <Box zIndex='banner' w='full' h='full' display='flex' justifyContent='flex-end' alignItems='center'>
-          <Image w='container.sm' h='auto' shadow='dark-lg' src='/images/code-example.png' alt='Example of running lyra on a JavaScript runtime' rounded='xl' />
+        <Box
+          label="banner"
+          zIndex="banner"
+          w="full"
+          h="full"
+          display="flex"
+          justifyContent="flex-end"
+          alignItems="center"
+        >
+          <Image
+            w="container"
+            h="auto"
+            shadow="dark-lg"
+            src="/images/code-example.png"
+            alt="Example of running lyra on a JavaScript runtime"
+            rounded="xl"
+          />
         </Box>
       </Box>
 
-      <Box w='full' bgGradient='linear(to-b, #5200FF, #080419)' mt='60'>
-        <Box w='container.xl' m='auto' py='20' display='grid' gridTemplateColumns='1fr 1fr' columnGap='16'>
+      <Box
+        px={["4", "0"]}
+        w="full"
+        bgGradient="linear(to-b, #5200FF, #080419)"
+        mt={["14", "60"]}
+      >
+        <Box
+          w="container"
+          m="auto"
+          py={["10", "20"]}
+          display="flex"
+          flexDirection="column"
+        >
           <Box>
-            <Heading> Why Lyra? </Heading>
-            <Text mt='10' textAlign='justify'>
-              Lyra is a modern, dependency-free full-text search engine written in TypeScript. <br />
-              It has been built with speed in mind and completes most search lookups in a few microseconds.<br /><br />
-
-              It implements a very fast, vanilla prefix tree to perform efficient lookups and easy serialization with multiple formats, such as <b>dpack</b>, <b>messagepack</b>, and <b>protocol buffers</b>.<br /><br />
-
-              Its main focus is to be able to run on edge networks, such as <b>AWS Lambda@Edge</b>, <b>Cloudflare Workers</b>, and <b>Netlify Functions</b>, so expect some updates on that.<br /><br />
-
-              Given that it's written in TypeScript, it can be used in <b>any</b> JavaScript runtime, including browsers, servers, React Native, and more.<br /><br />
-
-              It was named after the Lyra constellation due to its distributed and highly scalable nature.
+            <Heading mb="10"> Why Lyra? </Heading>
+            <Text textAlign="justify">
+              Lyra is a modern, dependency-free full-text search engine written
+              in TypeScript. <br />
+              It has been built with speed in mind and completes most search
+              lookups in a few microseconds.
+              <br />
+              <br />
+              It implements a very fast, vanilla prefix tree to perform
+              efficient lookups and easy serialization with multiple formats,
+              such as <b>dpack</b>, <b>messagepack</b>, and{" "}
+              <b>protocol buffers</b>.<br />
+              <br />
+              Its main focus is to be able to run on edge networks, such as{" "}
+              <b>AWS Lambda@Edge</b>, <b>Cloudflare Workers</b>, and{" "}
+              <b>Netlify Functions</b>, so expect some updates on that.
+              <br />
+              <br />
+              Given that it's written in TypeScript, it can be used in{" "}
+              <b>any</b> JavaScript runtime, including browsers, servers, React
+              Native, and more.
+              <br />
+              <br />
+              It was named after the Lyra constellation due to its distributed
+              and highly scalable nature.
             </Text>
           </Box>
 
@@ -114,28 +194,26 @@ const Home: NextPage = () => {
         </Box>
       </Box>
 
-      <Box w='full' mt='20'>
-        <Box w='container.xl' m='auto' pb='20'>
-          <Heading textAlign='center'> Runs everywhere </Heading>
-          <Grid templateColumns='repeat(8, 1fr)' mt='10'>
+      <Box w="full" mt={["0", "20"]}>
+        <Box w="container" m="auto" pb="20">
+          <Heading textAlign="center"> Runs everywhere </Heading>
+          <Grid templateColumns="repeat(8, 1fr)" mt="10">
             {supportedRuntimes.map((runtime) => (
               <Box key={runtime.name}>
-                <Tooltip hasArrow label={runtime.name} placement='top'>
-                  <Image
-                    src={runtime.image}
-                    alt={runtime.name}
-                    w='44'
-                  />
+                <Tooltip hasArrow label={runtime.name} placement="top">
+                  <Image src={runtime.image} alt={runtime.name} w="44" />
                 </Tooltip>
               </Box>
             ))}
           </Grid>
-          <Text textAlign='center' mt='10'> ...and any other JavaScript runtime. </Text>
+          <Text textAlign="center" mt="10">
+            {" "}
+            ...and any other JavaScript runtime.{" "}
+          </Text>
         </Box>
-
       </Box>
-    </>
-  )
-}
+    </Box>
+  );
+};
 
-export default Home
+export default Home;
