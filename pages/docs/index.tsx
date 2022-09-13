083eeaa33bea1d832c66a6e5f4308edf3a176b0f
Jaga Santagostino <jagasantagostino@gmail.com>
2022-09-13 17:50:50 +0200

    responsive docs index

--- pages/docs/index.tsx
+++ pages/docs/index.tsx
@@ -1,44 +1,71 @@
-import type { GetStaticProps } from 'next'
-import { Box, Grid, GridItem, Stack, Text } from '@chakra-ui/react'
-import { getAllPostsWithContent, PostsBySection } from '../../utils/docs/server-side'
-import Link from 'next/link'
+import type { GetStaticProps } from "next";
+import { Box, Flex, Grid, GridItem, Stack, Text } from "@chakra-ui/react";
+import {
+  getAllPostsWithContent,
+  PostsBySection,
+} from "../../utils/docs/server-side";
+import Link from "next/link";
 
 interface DocsProps {
-  posts: PostsBySection
+  posts: PostsBySection;
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const posts = await getAllPostsWithContent()
+  const posts = await getAllPostsWithContent();
 
   return {
     props: {
-      posts
-    }
-  }
-}
+      posts,
+    },
+  };
+};
 
-export default function Docs (props: DocsProps) {
-  const { posts } = props
-  const sections = Object.keys(posts)
+export default function Docs(props: DocsProps) {
+  const { posts } = props;
+  const sections = Object.keys(posts);
 
   return (
-    <Box w='container.xl' m='auto' py='40'>
-      <Box textAlign='center'>
-        <Text as='span' fontSize='5xl' fontWeight='bold' bgGradient='linear(to-l, #FF00E5, #00C8FF)' bgClip='text'> Lyra Documentation </Text>
+    <Box maxW="container.xl" m="auto" py={["24", "40"]} px="8">
+      <Box textAlign="center">
+        <Text
+          as="span"
+          fontSize={["4xl", "5xl"]}
+          fontWeight="bold"
+          bgGradient="linear(to-l, #FF00E5, #00C8FF)"
+          bgClip="text"
+        >
+          {" "}
+          Lyra Documentation{" "}
+        </Text>
       </Box>
-      <Grid gridTemplateColumns='1fr 1fr 1fr' mt='20' gap='10'>
+      <Flex direction={["column", "row"]} mt={["12", "20"]} gap="10">
         {sections.map((section) => (
-          <GridItem key={section}>
-            <Text as='h2' fontSize='2xl' fontWeight='bold' textTransform='capitalize' mb='5'> {section} </Text>
-            <Stack spacing='4'>
+          <Box key={section}>
+            <Text
+              as="h2"
+              fontSize="2xl"
+              fontWeight="bold"
+              textTransform="capitalize"
+              mb="5"
+            >
+              {" "}
+              {section}{" "}
+            </Text>
+            <Stack spacing="4">
               {posts[section].docs.map((post) => (
                 <Link href={post.slug} key={post.slug} passHref>
-                  <Text as='a'>
-                    <Box p='4' bgColor='gray.900' rounded='lg' transition='ease 0.3s' _hover={{ shadow: 'dark-lg', bgColor: 'gray.800' }}>
-                      <Text as='h2' fontWeight='semibold'>
+                  <Text as="a">
+                    <Box
+                      p="4"
+                      bgColor="gray.900"
+                      rounded="lg"
+                      transition="ease 0.3s"
+                      _hover={{ shadow: "dark-lg", bgColor: "gray.800" }}
+                    >
+                      <Text as="h2" fontWeight="semibold">
                         {post.data.title}
                       </Text>
-                      <Text as='p' color='gray.500'>
+                      <Text as="p" color="gray.500">
                         {post.data.description}
                       </Text>
                     </Box>
@@ -46,9 +73,9 @@ export default function Docs (props: DocsProps) {
                 </Link>
               ))}
             </Stack>
-          </GridItem>
+          </Box>
         ))}
-      </Grid>
+      </Flex>
     </Box>
-  )
+  );
 }
